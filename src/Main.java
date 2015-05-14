0d2fb341ba073575a56ecd57c6d6d5ace200f5ba
lucasawebb <lucasawebb@gmail.com>
2015-05-14 12:28:16 -0400

    Set up LWJGL

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 /**
  * Created by lucaswebb on 5/14/15.
  */
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class Main {
+    public Main() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+
+            while(!Display.isCloseRequested()) {
+                Display.update();
+            }
+
+            Display.destroy();
+        } catch(LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
 }
