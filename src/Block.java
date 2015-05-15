920b55981bd685aab3029c243eb3eae4bc6879a9
lucasawebb <lucasawebb@gmail.com>
2015-05-15 11:33:28 -0400

    Update to Block/Testing

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,5 @@
+import org.lwjgl.opengl.GL11;
+
 /**
  * Created by arjun on 5/15/15.
  */
@@ -9,6 +11,7 @@ public class Block {
     private int green;
     private int blue;
     private int alpha;
+    private boolean canRender = true;
     Block(int a, int b, int c, int d, int e, int f, int g) {
         x = a;
         y = b;
@@ -17,6 +20,62 @@ public class Block {
         green = e;
         blue = f;
         alpha = g;
+    }
+
+    public void render(){
+        if(canRender) {
+            GL11.glPushMatrix();
+            GL11.glTranslated(x, y, z);
+            cube(100);
+            GL11.glPopMatrix();
+        }
+    }
+
+    public void cube(int f){
+        GL11.glBegin(GL11.GL_QUADS);          // Begin drawing the color cube with 6 quads
+        // Top face (y = 1.0f)
+        // Define vertices in counter-clockwise (CCW) order with normal pointing out
+        GL11.glColor3f(0.0f, 1.0f, 0.0f);     // Green
+        GL11.glVertex3d( 1.0*f, 1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f, 1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f, 1.0*f,  1.0*f);
+        GL11.glVertex3d( 1.0*f, 1.0*f,  1.0*f);
+
+        // Bottom face (y = -1.0f)
+        GL11.glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+        GL11.glVertex3d( 1.0*f, -1.0*f,  1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f,  1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f, -1.0*f);
+        GL11.glVertex3d( 1.0*f, -1.0*f, -1.0*f);
+
+        // Front face  (z = 1.0f)
+        GL11.glColor3f(1.0f, 0.0f, 0.0f);     // Red
+        GL11.glVertex3d( 1.0*f,  1.0*f, 1.0*f);
+        GL11.glVertex3d(-1.0*f,  1.0*f, 1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f, 1.0*f);
+        GL11.glVertex3d( 1.0*f, -1.0*f, 1.0*f);
+
+        // Back face (z = -1.0f)
+        GL11.glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+        GL11.glVertex3d( 1.0*f, -1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f,  1.0*f, -1.0*f);
+        GL11.glVertex3d( 1.0*f,  1.0*f, -1.0*f);
+
+        // Left face (x = -1.0f)
+        GL11.glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+        GL11.glVertex3d(-1.0*f,  1.0*f,  1.0*f);
+        GL11.glVertex3d(-1.0*f,  1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f, -1.0*f);
+        GL11.glVertex3d(-1.0*f, -1.0*f,  1.0*f);
+
+        // Right face (x = 1.0f)
+        GL11.glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+        GL11.glVertex3d(1.0*f,  1.0*f, -1.0*f);
+        GL11.glVertex3d(1.0*f,  1.0*f,  1.0*f);
+        GL11.glVertex3d(1.0*f, -1.0*f,  1.0*f);
+        GL11.glVertex3d(1.0*f, -1.0*f, -1.0*f);
+        GL11.glEnd();  // End of drawing color-cube
     }
 
     public int getX() {
--- src/GameLoop.java
+++ src/GameLoop.java
@@ -35,12 +35,14 @@ public class GameLoop {
     public void initGL(){
         GL11.glMatrixMode(GL11.GL_PROJECTION);
         GL11.glLoadIdentity();
-        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glOrtho(0, 800, 0, 600, 600, -600);
         GL11.glMatrixMode(GL11.GL_MODELVIEW);
     }
 
-    public void renderGL(){
+    public void renderGL() {
         GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+        Block b = new Block(0,0,0,1,1,1,1);
+        b.render();
     }
 
     public static void main(String[] args){
