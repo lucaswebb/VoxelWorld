12a3934b53bd25a17b7652e95091fd0455de04b7
lucasawebb <lucasawebb@gmail.com>
2015-05-15 11:22:23 -0400

    Basic Gameloop Created

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -5,6 +5,7 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class GameLoop {
     public void start(){
@@ -15,6 +16,16 @@ public class GameLoop {
             e.printStackTrace();
             System.exit(0);
         }
+        initGL(); // init OpenGL
+
+        while (!Display.isCloseRequested()) {
+            renderGL();
+
+            Display.update();
+            Display.sync(60); // cap fps to 60fps
+        }
+
+        Display.destroy();
     }
 
     public void update(){
@@ -22,14 +33,18 @@ public class GameLoop {
     }
 
     public void initGL(){
-
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
     }
 
     public void renderGL(){
-
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
     }
 
     public static void main(String[] args){
-
+        GameLoop loop = new GameLoop();
+        loop.start();
     }
 }
