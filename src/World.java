e8a8de4f4b944acc1a08c0712d7b737d85ab345b
arajan1 <arajan1@exeter.edu>
2015-05-22 11:52:11 -0400

    World v!

---
+++ src/World.java
@@ -0,0 +1,39 @@
+/**
+ * Created by arjun on 5/21/15.
+ */
+import java.util.ArrayList;
+public class World {
+    private ArrayList<Chunk> chunks;
+    private int camarax = 0;
+    private int camaray = 0;
+    private int camaraz = 0;
+    World(){
+        chunks = new ArrayList<Chunk>();
+    }
+    public void setUp(){
+        chunks.add(new Chunk(0,0,0));
+        chunks.add(new Chunk(-1,0,0));
+        chunks.add(new Chunk(0,-1,0));
+        chunks.add(new Chunk(-1,-1,0));
+        for(Chunk chunk : chunks){
+            chunk.setUp();
+        }
+    }
+    public void setCamaraX(int a){
+        camarax = a;
+    }
+    public void setCamaraY(int a){
+        camaray = a;
+    }
+    public void setCamaraZ(int a){
+        camaraz = a;
+    }
+    public void render(){
+        for(Chunk chunk : chunks){
+            chunk.render();
+        }
+    }
+
+
+
+}
