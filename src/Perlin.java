1080c343c635fc3992304d268e40333afce40570
lucasawebb <lucasawebb@gmail.com>
2015-05-27 09:04:29 -0400

    Terrain gen

--- src/Perlin.java
+++ src/Perlin.java
@@ -8,13 +8,13 @@
 
 public class Perlin {
     public static final float P = 0.5f;
-    public static final int OCTAVES = 1;
+    public static final int OCTAVES = 8;
 
     public static float perlin2D(float x, float y) {
         float total = 0.0f;
 
         for (int i = 0; i < OCTAVES; i++) {
-            int frequency = (int) Math.pow(2, i/2);
+            int frequency = (int) Math.pow(2, i);
             float amplitude = (float) Math.pow(P, i);
 
             total += Noise2D.interpolate(x * frequency, y * frequency, Noise1D.primes[0]) * amplitude;
